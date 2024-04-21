76ef2e361160f49eb5b1867ec0d9e0c509b6c3e2
ranajoy <ranajoybhattacharjee19@gmail.com>
2024-04-21 11:41:57 +0530

    update

--- social_app/views.py
+++ social_app/views.py
@@ -2,14 +2,7 @@ from django.shortcuts import render
 
 
 def Home(request):
-    # Client secrets = a9ed1212f0e426fe211b4b19b63040aa8501a582
 
-    #Client ID =0d78b8cb386aa5cb476e
-
-    # google
-   #Client secrets= 667804701841-bn35u3c3g3v0sd53faos9grnio969nbj.apps.googleusercontent.com
-   
-    #Client ID = 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
     return render(request,"home.html")
 
 
